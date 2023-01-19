3b3feb8885abb04a6fa771bef001fdefa05fe74d
isana <eizisana@gmail.com>
2023-01-19 16:34:00 +0700

    remove import web vital

--- src/index.jsx
+++ src/index.jsx
@@ -4,8 +4,6 @@ import "./index.css";
 import App from "./App";
 import { AppProviders } from "./lib/components/app-provider";
 
-import reportWebVitals from "./reportWebVitals";
-
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
